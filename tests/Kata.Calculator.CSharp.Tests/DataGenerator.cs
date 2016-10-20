486510b500ab9c9d42554a69eff8e70d94e0dc5e
michal.niegrzybowski@gmail.com <michal.niegrzybowski@gmail.com>
2016-10-20 16:42:28 +0200

    final resolution

--- tests/Kata.Calculator.CSharp.Tests/DataGenerator.cs
+++ tests/Kata.Calculator.CSharp.Tests/DataGenerator.cs
@@ -7,41 +7,23 @@ namespace Kata.Calculator.CSharp.Tests
     {
         public const int MaximumConsideredValue = 1000;
 
-        public static Arbitrary<int[]> ValidInput()
-        {
-            var input = GetValidListOfIntegers();
-            return input.ToArbitrary();
-        }
-
-        public static Arbitrary<string> ValidSingleInput()
-        {
-            var input = Arb.Generate<PositiveInt>().Where(x => x.Get < MaximumConsideredValue)
-                .Select(x => x.ToString());
-            return input.ToArbitrary();
-        }
-
-        public static Arbitrary<CustomInputData> ValidCustomDelimiterData()
-        {
-            var input = from values in GetValidListOfIntegers()
-                        from delimiter in Arb.Generate<NonEmptyString>()
-                select new CustomInputData()
-                {
-                    Delimiter = delimiter.Get,
-                    Values = values
-                };
-            return input.ToArbitrary();
-        }
-
         public static Arbitrary<CustomInputData> CustomDelimiterWithAllValues()
         {
             var input = from values in Arb.Generate<PositiveInt[]>()
                         from delimiter in Arb.Generate<NonEmptyString>()
+                        where IsInt(delimiter.Item) == false
                         select new CustomInputData()
                         {
                             Delimiter = delimiter.Get,
                             Values = values.Select(x => x.Get).ToArray()
                         };
             return input.ToArbitrary();
+        }
+
+        private static bool IsInt(string value)
+        {
+            var parsedValue = 0;
+            return int.TryParse(value, out parsedValue);
         }
 
         public static Arbitrary<int[]> ValuesBiggerThan1000()
@@ -52,14 +34,6 @@ namespace Kata.Calculator.CSharp.Tests
                 .ToArray())
                 .ToArbitrary();
         } 
-
-        private static Gen<int[]> GetValidListOfIntegers()
-        {
-            return Arb.Generate<PositiveInt[]>()
-                .Select(x => x.Where(y => y.Get < MaximumConsideredValue)
-                .Select(y => y.Get)
-                .ToArray());
-        }
     }
 
     public class CustomInputData
